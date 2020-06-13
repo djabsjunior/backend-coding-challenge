11f3a406265a6e243b834f13840a9fce9be158d9
jbjunior <djabsjunior@gmail.com>
2020-06-13 16:58:54 -0400

    Fix lowercase issue

--- BackendCodingChallenge/BackendCodingChallenge/Calculators/LevenshteinDistance/LevenshteinDistanceCalculator.cs
+++ BackendCodingChallenge/BackendCodingChallenge/Calculators/LevenshteinDistance/LevenshteinDistanceCalculator.cs
@@ -3,22 +3,27 @@ namespace BackendCodingChallenge.Calculators.LevenshteinDistance
 {
     public class LevenshteinDistanceCalculator : ILevenshteinDistanceCalculator
     {
-        public int ComputeDistance(string req, string cityName)
+        public int ComputeDistance(string qName, string cityName)
         {
-            var reqLength = req.Length;
-            var cityNameLength = cityName.Length;
-            var d = new int[reqLength + 1, cityNameLength + 1];
+            //Lowercase both string before the algorithm evaluation
+            var lowerQName = qName.ToLower();
+            var lowerCityName = cityName.ToLower();
+            
+            //Levenshtein Algorithm
+            var qNameLength = lowerQName.Length;
+            var cityNameLength = lowerCityName.Length;
+            var d = new int[qNameLength + 1, cityNameLength + 1];
 
             // Step 1
             // returns the maximum length of both names if one is empty
-            if (Math.Min(reqLength, cityNameLength) == 0)
+            if (Math.Min(qNameLength, cityNameLength) == 0)
             {
-                return Math.Max(cityNameLength, reqLength);
+                return Math.Max(cityNameLength, qNameLength);
             }
 
             // Step 2
             // 2D arrays initialization of same length as both string names
-            for (var i = 0; i <= reqLength; d[i, 0] = i++)
+            for (var i = 0; i <= qNameLength; d[i, 0] = i++)
             {
             }
 
@@ -27,14 +32,14 @@ namespace BackendCodingChallenge.Calculators.LevenshteinDistance
             }
 
             // Step 3
-            for (var i = 1; i <= reqLength; i++)
+            for (var i = 1; i <= qNameLength; i++)
             {
                 //Step 4
                 for (var j = 1; j <= cityNameLength; j++)
                 {
                     // Step 5
                     // character substitution cost: set to 0 when a a change is needed and 1 otherwise.
-                    var cost = (cityName[j - 1] == req[i - 1]) ? 0 : 1;
+                    var cost = (lowerCityName[j - 1] == lowerQName[i - 1]) ? 0 : 1;
 
                     // Step 6
                     d[i, j] = Math.Min(
@@ -50,7 +55,7 @@ namespace BackendCodingChallenge.Calculators.LevenshteinDistance
                 }
             }
             // Step 7
-            return d[reqLength, cityNameLength]; //the last matrix position contains the distance
+            return d[qNameLength, cityNameLength]; //the last matrix position contains the distance
         }
     }
 }
